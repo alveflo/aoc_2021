ff63ff70075d82de8e2a98653a141eadff344f99
victor <victor.alveflo@gmail.com>
2021-12-06 19:49:04 +0100

    Update day06

--- day06/Program.cs
+++ day06/Program.cs
@@ -1,40 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
-List<int> parseInput(string filename)
-    => System.IO.File.ReadLines(filename)
-    .First()
-    .Split(",")
-    .Select(line => Int32.Parse(line))
-    .ToList();
-
-long solve(List<int> input, int days)
-{
-    var currentDay = 0;
+List<int> parseInput(string filename) => System.IO.File.ReadLines(filename).First().Split(",").Select(line => Int32.Parse(line)).ToList();
+long solve(List<int> input, int days) {
     var fishes = new long[9];
-
     foreach (var i in input) fishes[i]++;
-
-    while (currentDay < days) {
+    for (var day = 0; day < days; day++) {
         var newFishes = fishes[0];
-        var temp = new long[fishes.Length];
-        for (var i = 0; i < temp.Length-1; i++)
-            temp[i] = fishes[i+1];
-
-        temp[6] += newFishes;
-        temp[8] += newFishes;
-
-        fishes = temp;
-        currentDay++;
+        for (var i = 0; i < fishes.Length - 1; i++) fishes[i] = fishes[i+1];
+        fishes[6] += newFishes;
+        fishes[8] = newFishes;
     }
-
     return fishes.ToList().Sum();
 }
-
 var input = parseInput("input.txt");
-
-var part = Environment.GetEnvironmentVariable("part");
-
-Console.WriteLine("C#");
-Console.WriteLine(solve(input, part == "part2" ? 256 : 80));
+Console.WriteLine(solve(input, Environment.GetEnvironmentVariable("part") == "part2" ? 256 : 80));
